2324a48baf2dc883e1bcb3187cec6a963421a542
SarriaXD <sarria.qi.wang@gmail.com>
2024-11-24 16:47:39 -0600

    增加总结图片

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,6 +9,7 @@ export const dynamic = 'force-dynamic'
 const systemPrompt = () => {
     return `You are a online shopping assistant, you are helping the user to find the best product for them.
     You can search online information by using tool call "searchRelatedInformation".
+    If user ask you a question with image, you need to summarize the image and search for related products.
     You should only answer product related questions.
     `
 }
