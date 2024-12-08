6f4b4f27bf6b6dbcc5acb2715dcf0fea8af0fc0f
SarriaXD <sarria.qi.wang@gmail.com>
2024-12-08 13:07:31 -0600

    实现rag

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,14 +1,15 @@
 import { convertToCoreMessages, streamText, tool } from 'ai'
 import { openai } from '@ai-sdk/openai'
 import { z } from 'zod'
-import searchRelatedInformation from '@/lib/service/utils/search-related-information-utils.ts'
+import { findRelevantProducts } from '@/lib/ai/find-relevantProducts.ts'
 
 export const maxDuration = 30
 export const dynamic = 'force-dynamic'
 
 const systemPrompt = () => {
     return `You are a online shopping assistant, you are helping the user to find the best product for them.
-    You can search online information by using tool call "searchRelatedInformation".
+    You can search relevant products by using tool call "findRelevantProducts" and you should translate user's question into english as query param.
+    For each tool call, you should check the tool call result and only return the relevant information based on user's question and tool call result.
     If user ask you a question with image, you need to summarize the image and search for related products.
     You should only answer product related questions.
     `
@@ -25,7 +26,7 @@ export async function POST(request: Request) {
         messages: convertToCoreMessages(messages),
         maxSteps: 5,
         tools: {
-            searchRelatedInformation: tool({
+            findRelevantProducts: tool({
                 description: 'Search for related products',
                 parameters: z.object({
                     description: z
@@ -34,8 +35,10 @@ export async function POST(request: Request) {
                             'the description of the product you want to search',
                         ),
                 }),
-                execute: async () => {
-                   return await searchRelatedInformation()
+                execute: async (query) => {
+                    const result = await findRelevantProducts(query.description)
+                    console.log('query:', query, 'result', result)
+                    return result
                 },
             }),
         },
---
+++ src/lib/ai/find-relevantProducts.ts
@@ -0,0 +1,37 @@
+import { embed } from 'ai'
+import { openai } from '@ai-sdk/openai'
+import { db } from '../db'
+import { cosineDistance, desc, eq, gt, sql } from 'drizzle-orm'
+import { embeddings } from '../db/schema/embeddings'
+import { products } from '@/lib/db/schema/products.ts'
+
+const embeddingModel = openai.embedding('text-embedding-3-small');
+
+const generateEmbedding = async (value: string): Promise<number[]> => {
+    const input = value.replace('\\n', ' ');
+    const { embedding } = await embed({
+        model: embeddingModel,
+        value: input,
+    });
+    return embedding;
+};
+
+
+export const findRelevantProducts = async (userQuery: string) => {
+    const userQueryEmbedded = await generateEmbedding(userQuery);
+    const similarity = sql<number>`1 - (${cosineDistance(
+        embeddings.embedding,
+        userQueryEmbedded,
+    )})`;
+    return db
+        .select({
+            product: products,  // 选择整个 product
+            similarity
+        })
+        .from(embeddings)
+        .innerJoin(products, eq(embeddings.productAsin, products.asin))  // 通过 asin 连接
+        .where(gt(similarity, 0.5))
+        .orderBy(desc(similarity))
+        .limit(4)
+};
+
